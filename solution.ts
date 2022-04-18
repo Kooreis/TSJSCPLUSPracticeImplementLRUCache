5a0f6a36af351c08d2a4003b03b84c0b5f2dfbf4
Kooreis <kooreis.official@gmail.com>
2022-04-17 23:33:47 -0500

    This update adds the 'get' method to the LRUCache class. This method retrieves the value of a given key if it exists in the cache. If the key does not exist, it returns -1. It also updates the position of the key in the cache to mark it as recently used.

--- solution.ts
+++ solution.ts
@@ -1,9 +1,9 @@
-class LRUCache {
-    capacity: number;
-    cache: Map<number, number>;
-
-    constructor(capacity: number) {
-        this.capacity = capacity;
-        this.cache = new Map();
+get(key: number): number {
+        if (!this.cache.has(key)) {
+            return -1;
+        }
+        const temp = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, temp);
+        return temp;
     }
-}
