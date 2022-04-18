8638a11aea370f5bb7237c825f41e9fb719ac165
Kooreis <kooreis.official@gmail.com>
2022-04-17 23:33:47 -0500

    This update adds the 'get' method to the LRUCache class. This method checks if a key exists in the cache. If it does, it removes the key and reinserts it to update its position in the insertion order, then returns the value. If the key does not exist, it returns -1.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,9 @@
-class LRUCache {
-    constructor(capacity) {
-        this.capacity = capacity;
-        this.cache = new Map();
+get(key) {
+        if (!this.cache.has(key)) {
+            return -1;
+        }
+        const temp = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, temp);
+        return temp;
     }
-}
